0475436a85573ff2beb5456cd7e3b171314a246b
Trevour <ambiatrevour2023@gmail.com>
2025-03-08 10:21:13 +0300

    work done

---
+++ index.js
@@ -0,0 +1,12 @@
+const foo = function() {
+    return 'bar';
+  }
+  const add = (a, b) => a + b;
+  const twoAdder = x => x + 2;
+  const nums = [1, 2, 3];
+  const squares = nums.map(x => x ** 2);  // [1, 4, 9]
+const divide = function() {
+    return 2000 / 100;
+}
+
+const square = x => x * x;
